accf89f1f4ab021e57c9c4c168bb46079318df3a
Your Name <you@example.com>
2025-05-18 00:12:43 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -89,39 +89,32 @@ List* get_adj_nodes(Node* n){
    List* list = createList();
 
    int i, j;
-   int found = 0; // Variable para marcar si se encontró un espacio vacío
-   for (i = 0; i < 9 && !found; i++) { // Buscar la primera fila con un espacio vacío
+   int found = 0;
+   for (i = 0; i < 9 && !found; i++) {
       for (j = 0; j < 9; j++) {
          if (n->sudo[i][j] == 0) {
-            found = 1; // Se encontró un espacio vacío
+            found = 1;
             break;
          }
       }
    }
 
-   if (!found) return list; // Si no se encontró un espacio vacío, retornar lista vacía
+   if (!found) return list;
 
-   i--; // Ajustar i y j a la posición del espacio vacío
+   i--;
 
-   for (int k = 1; k <= 9; k++) { // Probar números del 1 al 9
-      Node* new_node = copy(n); 
-      new_node->sudo[i][j] = k; // Asignar el número k al espacio vacío
-      pushBack(list, new_node);
+   for (int k = 1; k <= 9; k++) {
+      Node* new_node = copy(n);
+      new_node->sudo[i][j] = k;
+
+      if (is_valid(new_node)) {
+          pushBack(list, new_node);
+      } else {
+          free(new_node);
+      }
    }
 
    return list;
-}
-
-
-
-
-int is_final(Node* n){
-   for (int i = 0; i < 9; i++) { // Itera por todas las filas
-      for (int j = 0; j < 9; j++) { // Itera por todas las columnas
-         if (n->sudo[i][j] == 0) return 0; // Si hay un espacio vacío, es nulo
-      }
-   }
-   return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
@@ -130,24 +123,28 @@ Node* DFS(Node* initial, int* cont){
     *cont = 0;
 
     while (!is_empty(S)) {
-        Node* current = top(S); // Obtener el nodo del tope
-        pop(S);                 // Sacarlo de la pila
+        Node* current = top(S);
+        pop(S);
         (*cont)++;
 
         if (is_final(current)) {
-            return current;    // Fin
+            if (is_valid(current)) { 
+                return current;
+            } else {
+                free(current);
+                continue;
+            }
         }
 
-        List* adj = get_adj_nodes(current); // Obtener nodos adyacentes
+        List* adj = get_adj_nodes(current);
         Node* adjNode = first(adj);
 
         while (adjNode != NULL) {
-            push(S, adjNode); // Agregar nodo a la pila
+            push(S, adjNode);
             adjNode = next(adj);
         }
 
         free(current);
     }
 
-    return NULL; 
-}
+    return NULL;
